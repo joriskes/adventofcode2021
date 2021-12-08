e9ee9b9c78cd51a401fd341f8ac32284d98cf331
Joris <49513527+joriskes@users.noreply.github.com>
2021-12-08 17:49:41 +0100

    Add runall timer

--- runall.php
+++ runall.php
@@ -12,7 +12,10 @@ for ($i = 1; $i <= 25; $i++) {
 $executionStartTime = microtime(true);
 foreach ($torun as $running) {
     echo $running['title'] . "\n";
+    $dayStartTime = microtime(true);
     include $running['file'];
+    $dayTime = number_format((microtime(true) - $dayStartTime) * 1000, 3, '.', '') . 'ms';
+    echo 'Day time ' . $dayTime . "\n\n";
 }
 $executionTime = number_format((microtime(true) - $executionStartTime) * 1000, 3, '.', '') . 'ms';
 echo 'Total time: ' . $executionTime . "\n";
