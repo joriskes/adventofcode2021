b4b903e52eb37ea070d986c7814824bc9c045525
Joris <49513527+joriskes@users.noreply.github.com>
2021-12-11 13:51:45 +0100

    Day 9

---
+++ day9/run.php
@@ -0,0 +1,60 @@
+<?php
+require __DIR__ . '/../bootstrap.php';
+
+$input = file_get_contents(__DIR__ . '/input.txt');
+$lines = input_to_lines($input);
+foreach ($lines as $line) {
+    $floor[] = array_map('intval', str_split($line));
+}
+
+function val($floor, $x, $y)
+{
+    $height = count($floor);
+    $width = count($floor[0]);
+    if ($x >= 0 && $x < $width && $y >= 0 && $y < $height) {
+        return $floor[$y][$x];
+    }
+    return 9;
+}
+
+$counted = [];
+function count_basin($floor, $x, $y)
+{
+    global $counted;
+    if (in_array($x . ',' . $y, $counted)) {
+        return 0;
+    }
+    $counted[] = $x . ',' . $y;
+    $h = val($floor, $x, $y);
+    if ($h > 8) {
+        return 0;
+    }
+    $sum = 1;
+    if (val($floor, $x + 1, $y) > $h) $sum += count_basin($floor, $x + 1, $y);
+    if (val($floor, $x - 1, $y) > $h) $sum += count_basin($floor, $x - 1, $y);
+    if (val($floor, $x, $y + 1) > $h) $sum += count_basin($floor, $x, $y + 1);
+    if (val($floor, $x, $y - 1) > $h) $sum += count_basin($floor, $x, $y - 1);
+    return $sum;
+}
+
+$part1 = 0;
+$part2 = 0;
+
+$basins = [];
+foreach ($floor as $y => $f) {
+    foreach ($f as $x => $h) {
+        if ($h < val($floor, $x + 1, $y) &&
+            $h < val($floor, $x - 1, $y) &&
+            $h < val($floor, $x, $y + 1) &&
+            $h < val($floor, $x, $y - 1)) {
+            $part1 += $h + 1;
+
+            $basins[] = count_basin($floor, $x, $y);
+        }
+    }
+}
+rsort($basins);
+$part2 = $basins[0] * $basins[1] * $basins[2];
+
+p('Part 1: ' . $part1);
+p('Part 2: ' . $part2);
--- utils.php
+++ utils.php
@@ -25,3 +25,26 @@ function input_first_line_to_int_array($input)
     $line = trim(array_pop($lines));
     return array_map('intval', explode(',', $line));
 }
+
+class color
+{
+    public const black = '30';
+    public const red = '31';
+    public const green = '32';
+    public const brown = '33';
+    public const blue = '34';
+    public const purple = '35';
+    public const cyan = '36';
+    public const white = '37';
+}
+
+/**
+ * Returns a string console colorized to the supplied color
+ * @param string $color (color::black)
+ * @param string $str
+ * @return string
+ */
+function console_color($color, $str)
+{
+    return "\033[" . $color . "m" . $str . "\033[37m";
+}
