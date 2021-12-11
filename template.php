8a3454144ef4e94e111a301d158c1191b106670b
Joris <49513527+joriskes@users.noreply.github.com>
2021-12-11 13:52:28 +0100

    Fix template

--- template.php
+++ template.php
@@ -1,5 +1,7 @@
 <?php
-require __DIR__.'/../bootstrap.php';
+require __DIR__ . '/../bootstrap.php';
 
-$input = file_get_contents(__DIR__.'/input.txt');
+$input = file_get_contents(__DIR__ . '/input.txt');
 
+//p('Part 1: ' . $part1);
+//p('Part 2: ' . $part2);
